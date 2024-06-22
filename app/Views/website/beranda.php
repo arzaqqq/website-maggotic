401d25ab11c7ac625d81349f02f606376833a2f3
arzaqqq <mmiftahularzaq01@gmail.com>
2024-06-22 22:17:21 +0700

    first-commit

---
+++ app/Views/website/beranda.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate atque consectetur excepturi asperiores
+    quibusdam ab similique a hic libero, quam exercitationem eveniet aliquid, ea ipsam rerum mollitia officiis commodi
+    at ipsa optio? Eos consequatur quam est corporis optio, praesentium eveniet ipsam voluptate repellat tempore, omnis
+    adipisci molestiae minima? Quae, esse! Iusto reprehenderit eligendi, alias debitis autem voluptas nam quo fugiat.
+    Quae neque quasi nam repellat beatae eius eveniet amet consectetur iste incidunt nesciunt, voluptatum recusandae
+    doloremque voluptatem, architecto rerum pariatur, nisi explicabo aspernatur saepe autem temporibus alias. Fuga,
+    veritatis impedit vero nisi expedita, optio cupiditate saepe dolores, nobis consectetur debitis.</div>
+<?= $this->endsection('content'); ?>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/Views/*.php",
+    "./app/Views/**/*.php",
+    "./app/Views/**/**/*.php",
+    "./app/Views/**/**/**/*.php",
+  ],
+  
+  theme: {
+    extend: {
+      colors: {
+        'maggotic':'#1E90FF'
+      }
+    },
+  },
+  plugins: [],
+}
+
