020e4ff2a737039ecb138efb6a11a3c156df0959
arzaqqq <mmiftahularzaq01@gmail.com>
2024-06-25 10:41:34 +0700

    commit : beranda dan smartgot

--- app/Views/website/beranda.php
+++ app/Views/website/beranda.php
@@ -1,5 +1,45 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-<div class=""></div>
+
+<div class="flex justify-center align-center mt-20 mb-10">
+    <h1 class=" text-maggotic font-bold text-3xl">MAGGOTIC</h1>
+</div>
+
+
+<div class="container mx-auto">
+    <div class="flex justify-center align-content-enter">
+        <div class="w-[40%] my-auto text-center">
+            <h2 class="text-[25px] font-bold">Selamat Datang di website Maggotic</h2>
+            <p class="text-[25px]">Sebuah platform yang dirancang untuk tempat di mana kami menggabungkan keajaiban
+                maggot dengan inovasi
+                teknologi. Temukan berbagai informasi tentang penjualan maggot, teknologi terkini, dan solusi
+                berkelanjutan.
+            </p>
+            <button class=" bg-maggotic text-white rounded-full px-4 py-1 mt-3">Produk</button>
+        </div>
+        <div class="w-[40%]">
+            <img src="/img/foto-maggot.png" alt="Foto Maggot" class="">
+        </div>
+    </div>
+</div>
+
+
+<div class="container mt-24 mx-auto w-3/4 mb-24">
+    <div class="flex justify-center items-center">
+        <img src="/img/foto-produk-maggot.png" alt="Foto Produk Maggot" class="w-[1168px]">
+    </div>
+    <div class="flex justify-center items-center bg-[#2C2C2C]">
+        <p class="text-center p-8 text-white">Maggotic merupakan sebuah aplikasi yang menjadi inovasi platform
+            pemasaran maggot
+            BSF
+            berbasis syariah.
+            Maggotic juga hadir dalam membantu pemerintah dan masyarakat untuk mendapatkan pendapatan tambahan melalui
+            sampah organik, pengembangan model budidaya maggot BSF, dan meningkatkan penjualan hasil ternak melalui
+            pakan maggot yang berkualitas, sehat, dan tidak mengandung patogen. Konsumen juga dapat memilih berbagai
+            varian produk original dan hasil olahan maggot.</p>
+    </div>
+</div>
+
+
 <?= $this->endsection('content'); ?>
--- app/Views/website/smartgot.php
+++ app/Views/website/smartgot.php
@@ -1,5 +1,98 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-<div class=""></div>
-<?= $this->endsection('content'); ?>
+<div class="flex justify-center mt-10 font-bold text-3xl">
+    <h1>Berita Maggot</h1>
+</div>
+
+<div class="container mx-auto mt-8">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <!-- Card 1 -->
+        <div class="card bg-base-100 w-full h-[542px] shadow-lg">
+            <figure>
+                <img src="/img/foto-manfaat-maggot.png" alt="Manfaat Maggot" class="w-full object-cover" />
+            </figure>
+            <div class="card-body px-4 py-5">
+                <h2 class="card-title font-bold text-lg">Manfaat Maggot</h2>
+                <p class="text-base">Maggot memiliki sejuta manfaat yang menarik dan bermanfaat, terutama dalam konteks
+                    lingkungan.</p>
+                <div class="card-actions flex justify-end mt-4">
+                    <button class="px-4 py-2 rounded-full border-2 border-stone-950">Selengkapnya</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Card 2 -->
+        <div class="card bg-base-100 w-full h-[542px] shadow-lg">
+            <figure>
+                <img src="/img/masalah-sampah.png" alt="Manfaat Maggot" class="w-full object-cover" />
+            </figure>
+            <div class="card-body px-4 py-5">
+                <h2 class="card-title font-bold text-lg">Masalah sampah?</h2>
+                <p class="text-base">Sampah di Indonesia masih menjadi masalah yang belum terselesaikan dengan baik.
+                    Budidaya maggot...</p>
+                <div class="card-actions flex justify-end mt-4">
+                    <button class="px-4 py-2 rounded-full border-2 border-stone-950">Selengkapnya</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Card 3 -->
+        <div class="card bg-base-100 w-full h-[542px] shadow-lg">
+            <figure>
+                <img src="/img/foto-pakan-ayam.png" alt="Manfaat Maggot" class="w-full object-cover" />
+            </figure>
+            <div class="card-body px-4 py-5">
+                <h2 class="card-title font-bold text-lg">Maggot sebagai pakan ayam?</h2>
+                <p class="text-base">Maggot dapat menjadi pakan ternak yang bergizi tinggi.....</p>
+                <div class="card-actions flex justify-end mt-4">
+                    <button class="px-4 py-2 rounded-full border-2 border-stone-950">Selengkapnya</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Card 4 -->
+        <div class="card bg-base-100 w-full h-[542px] shadow-lg">
+            <figure>
+                <img src="/img/foto-maggot-hebat.png" alt="Manfaat Maggot" class="w-full object-cover" />
+            </figure>
+            <div class="card-body px-4 py-5">
+                <h2 class="card-title font-bold text-lg">Maggot sebagai pakan?</h2>
+                <p class="text-base">Maggot merubah hidup santoso, ia mengolah maggot menjadi...</p>
+                <div class="card-actions flex justify-end mt-4">
+                    <button class="px-4 py-2 rounded-full border-2 border-stone-950">Selengkapnya</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Card 5 -->
+        <div class="card bg-base-100 w-full h-[542px] shadow-lg">
+            <figure>
+                <img src="/img/foto-budidaya-maggot.png" alt="Manfaat Maggot" class="w-full object-cover" />
+            </figure>
+            <div class="card-body px-4 py-5">
+                <h2 class="card-title font-bold text-lg">Cara budidaya maggot</h2>
+                <p class="text-base">Budidaya maggot sangatlah mudah, cukup sediakan......</p>
+                <div class="card-actions flex justify-end mt-4">
+                    <button class="px-4 py-2 rounded-full border-2 border-stone-950">Selengkapnya</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Card 6 -->
+        <div class="card bg-base-100 w-full h-[542px] shadow-lg">
+            <figure>
+                <img src="/img/foto-makanan-basi.png" alt="Manfaat Maggot" class="w-full object-cover" />
+            </figure>
+            <div class="card-body px-4 py-5">
+                <h2 class="card-title font-bold text-lg">Punya makanan basi?</h2>
+                <p class="text-base">Jangan dibuang sembarangan, berikan makanan basi anda pada maggot maka akan....</p>
+                <div class="card-actions flex justify-end mt-4">
+                    <button class="px-4 py-2 rounded-full border-2 border-stone-950">Selengkapnya</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection('content'); ?>
